ad7f3b2415a39aa926c7b36ccc80387996d34024
Puru Vijay <devpuruvj@gmail.com>
2021-09-07 13:10:26 +0530

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import staticAdapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		adapter: staticAdapter(),
+	},
+};
+
+export default config;
